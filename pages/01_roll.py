acdd9e2e94c8d9b923102eea4180b1f6bc73a3ec
soave424 <soave424@gmail.com>
2024-11-01 00:37:38 +0900

    roll edit

--- pages/01_roll.py
+++ pages/01_roll.py
@@ -1,20 +1,11 @@
 import streamlit as st
-import pandas as pd
 import os
 
-# 환경 변수에서 CSV 파일 경로 가져오기
+# CSV 파일 경로 가져오기
 CSV_PATH = st.secrets["CSV_FILE_PATH"]
 
-@st.cache_data
-def load_data():
-    if os.path.exists(CSV_PATH):
-        return pd.read_csv(CSV_PATH)
-    else:
-        st.error("CSV 파일을 찾을 수 없습니다. 경로를 확인해주세요.")
-        return pd.DataFrame()
-
-# 데이터 로드
-data = load_data()
-
-st.write("현재 작업 디렉토리:", os.getcwd())
-st.write("현재 디렉토리의 파일 목록:", os.listdir('.'))
+# CSV 파일 경로 확인
+if not os.path.exists(CSV_PATH):
+    st.error("CSV 파일을 찾을 수 없습니다. 경로를 확인해주세요.")
+else:
+    st.write("CSV 파일 경로:", CSV_PATH)
