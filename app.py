ed37aeac60480002f2e74ebe7de9df5904e26c70
soave424 <soave424@gmail.com>
2024-11-01 01:21:32 +0900

    버튼 사이즈 조정

--- app.py
+++ app.py
@@ -11,8 +11,6 @@ st.set_page_config(
     initial_sidebar_state="collapsed"
 )
 
-
-
 # 탭 생성
 tab1, tab2, tab3 = st.tabs(["공지", "강좌 정보", "찾아오는 길"])
 
@@ -23,6 +21,14 @@ with tab1:
     # 버튼 스타일 추가
     st.markdown("""
         <style>
+            .button-container {
+                display: flex;
+                justify-content: space-between;
+                align-items: center;
+                gap: 10px;
+                margin-top: 20px;
+                margin-bottom: 40px;
+            }
             .button-link {
                 background-color: #5eb4d6;
                 color: white !important;
@@ -31,10 +37,9 @@ with tab1:
                 padding: 10px 20px;
                 font-size: 1.1rem;
                 font-weight: bold;
-                cursor: pointer;
                 text-align: center;
                 text-decoration: none;
-                width: 100%;
+                flex: 1;
             }
             .button-link:hover {
                 background-color: #4ca2bf;
@@ -42,24 +47,17 @@ with tab1:
         </style>
     """, unsafe_allow_html=True)
 
-    # 공지 사항 버튼들
-    col1, col2, col3 = st.columns(3)
-
-    with col1:
-        st.markdown(
-            '<a class="button-link" href="https://241109.streamlit.app/roll" target="_self">강좌신청 확인하기🔍</a>',
-            unsafe_allow_html=True
-        )
-    with col2:
-        st.markdown(
-            '<a class="button-link" href="https://open.kakao.com/o/g141aCVg" target="_blank">오픈채팅방 입장👨🏻‍💻</a>',
-            unsafe_allow_html=True
-        )
-    with col3:
-        st.markdown(
-            '<a class="button-link" href="https://bit.ly/econo1109" target="_blank">연수후기 남기기🖼</a>',
-            unsafe_allow_html=True
-        )
+    # 버튼 3개를 한 줄에 꽉 차게 배치
+    st.markdown(
+        """
+        <div class="button-container">
+            <a class="button-link" href="https://241109.streamlit.app/roll" target="_self">강좌신청 확인하기🔍</a>
+            <a class="button-link" href="https://open.kakao.com/o/g141aCVg" target="_blank">오픈채팅방 입장👨🏻‍💻</a>
+            <a class="button-link" href="https://bit.ly/econo1109" target="_blank">연수후기 남기기🖼</a>
+        </div>
+        """,
+        unsafe_allow_html=True
+    )
 
     # 일시 및 장소
     st.subheader("📅 일시 및 장소")
@@ -72,16 +70,6 @@ with tab1:
     지하철 이용 시: 여의도역(5, 9호선) 4번 출구 이용
     """)
     st.image("image/map.png", caption="", use_column_width=True)
-
-    st.markdown("""
-    ✅ **점심 식사**  
-    참가 확정 후 희망하는 분에 한해 도시락 및 근처 식당 예약을 받을 예정입니다.
-
-    ✅ **준비물**  
-    필기도구를 준비해주세요. 몇몇 강의는 별도의 강의안을 인쇄하여 제공할 예정입니다.  
-    전체 교안과 PPT는 따로 제공되지 않습니다.
-    """)
-    st.image("image/1.jpg", caption="연수안내", use_column_width=True)
 
 # 탭 2: 강좌 정보
 with tab2:
