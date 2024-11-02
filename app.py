6602f44af2679eafd293e2c4006abf499ef8c77f
soave424 <soave424@gmail.com>
2024-11-02 22:23:10 +0900

    a

--- app.py
+++ app.py
@@ -63,12 +63,12 @@ def logout():
 def render_sidebar():
     with st.sidebar:
         if not st.session_state.is_logged_in:
-            st.radio("로그인 유형 선택", ["연수 참여", "강사", "운영지원"], key="user_type_selection")
+            st.radio("로그인 유형 선택", ["연수참여", "강사", "운영지원"], key="user_type_selection")
             st.text_input("이름", key="input_name")
             st.text_input("코드", key="input_code", type="password")
             st.button("로그인", on_click=login)
         else:
-            if st.session_state.user_type == "연수 참여":
+            if st.session_state.user_type == "연수참여":
                 st.sidebar.success(f"{st.session_state.name} 선생님! 경금교 연수에 오신 것을 환영합니다.")
             elif st.session_state.user_type == "강사":
                 st.sidebar.success(f"{st.session_state.name} 선생님! 오늘 연수 힘내세요!")
